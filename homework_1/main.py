bad3f5dd6eda17925ec44f7b0a21ca7201d58d55
Quargam <kapylovmacim@mail.ru>
2022-07-22 00:13:06 +0300

    added comments

--- homework_1/main.py
+++ homework_1/main.py
@@ -37,7 +37,7 @@ class Dataset:
                             dtype=np.float64) + np.random.randn(N) * (D / random_dist_points)  # theta
             r = radius * np.sin(petals * t) + np.random.randn(N) * 0.2
             X[ix] = np.c_[r * np.sin(t), r * np.cos(t)]
-            Y[ix] = j
+            Y[ix] = j / float(dimensionality - 1)
 
         return X, Y  # X - coordinates of points; Y - color of points
 
@@ -48,7 +48,7 @@ class Dataset:
         plt.show()
 
 
-def plot_decision_boundary(model, X, y):
+def plot_decision_boundary(model, X, y, dimensionality = 1):
     # Set min and max values and give it some padding
     X = X.T
     x_min, x_max = X[0, :].min() - 1, X[0, :].max() + 1
@@ -63,7 +63,7 @@ def plot_decision_boundary(model, X, y):
     plt.contourf(xx, yy, Z, cmap=plt.cm.Spectral)
     plt.ylabel('x2')
     plt.xlabel('x1')
-    plt.scatter(X[0, :], X[1, :], c=y, cmap=plt.cm.Spectral)
+    plt.scatter(X[0, :], X[1, :], c=y*dimensionality, cmap=plt.cm.Spectral)
     plt.show()
 
 
@@ -74,6 +74,12 @@ class LayerNN:
 
     def __init__(self, n_input: int, lot_of_neuron: int, activation_func=lambda x: x,
                  deriv_activation_func=lambda x: 1):
+        """
+        :param n_input: number of input features for this layer
+        :param lot_of_neuron: number of layer neurons
+        :param activation_func: activation function
+        :param deriv_activation_func: gradient activation function
+        """
         self.lot_of_neuron = lot_of_neuron
         self.n_input = n_input
         self.activation_func = activation_func  # activation function
@@ -134,23 +140,37 @@ class LayerNN:
         return a1
 
     def backward(self, dL):
-
+        """
+        calculates the gradient for the layer
+        :param dL:
+        :return:
+        """
         z1, a1, X = self.unpack_cache(['z1', 'a1', 'X'])
-        # print(f'X:{X.shape},z1:{z1.shape},a1:{a1.shape},dL:{dL.shape}')
         dz1 = dL * self.deriv_activation_func(a1)
         dw1 = dL.T @ X
         db1 = dL.T @ np.ones((z1.shape[0]))
         self.collect_grads(dw1=dw1, db1=db1)
-        # print(f'dz1:{dz1.shape},dw{dw1.shape}:,db:{db1.shape}')
         return dz1 @ self.params["w1"]
 
 
 class Optimizer:
-    def __init__(self, regression_class, lr=1e-2):
-        self.model = regression_class
+    """
+    changes weights and b
+    """
+
+    def __init__(self, model, lr=1e-2):
+        """
+        :param model: neural network object
+        :param lr: assessment of training
+        """
+        self.model = model
         self.lr = lr
 
     def step(self):
+        """
+        updates weights and b by gradient descent
+        :return: None
+        """
         for key in self.model.neural_network.keys():
             old_params = self.model.neural_network[key].params
             new_params = {
@@ -163,6 +183,10 @@ class Optimizer:
             self.model.neural_network[key].params = new_params
 
     def zero_grad(self):
+        """
+        zeroes the gradient values
+        :return: None
+        """
         for _, key in enumerate(self.model.neural_network.keys(), 0):
             for i in self.model.neural_network[key].gradients.keys():
                 self.model.neural_network[key].gradients[i] = np.zeros_like(self.model.neural_network[key].gradients[i])
@@ -175,24 +199,23 @@ class MultilayerNN:
 
     def __init__(self, param_neural_network: dict, n_input: int, ):
         """
-        :param param_neural_network: a dictionary that describes the number of layers, neurons, and activation functions
-        example: neural_network = {"layer_1": [2, 'sigmoid', 'deriv_sigmoid'],
-        'layer_2': [3, 'sigmoid', 'deriv_sigmoid'], 'last_layer_3': [1, 'sigmoid', 'deriv_sigmoid']}
-        :param  n_input: number of input parameters
-        :param optimizer:
+        :param param_neural_network: a dictionary that describes the number of layers, neurons, activation functions and
+                                                                               gradient activation functions
+        example: neural_network = {'layer_1': [10, 'sigmoid', 'deriv_sigmoid'],
+                                   'layer_2': [30, 'sigmoid', 'deriv_sigmoid'],
+                                   'last_layer_3': [1, 'sigmoid', 'deriv_sigmoid']}
+        :param  n_input: number of input parameters(number of features)
         """
-        self.param_neural_network = param_neural_network
-        self.lot_of_layer = len(param_neural_network)
-        self.n_input = n_input
+        self.param_neural_network = param_neural_network  # neural network parameters
+        self.n_input = n_input  # number of features
         self.neural_network = {}  # stored layers of the neural network
-        self.outputNN = None
+        self.outputNN = None  # the vector of values that the neural network outputs
         self._initialize_layers(self.param_neural_network, self.n_input)
 
     def _initialize_layers(self, param_neural_network, n_input):
         """
-        :param param_neural_network:
-        :param n_input: DOTO: исправить
-        :return:
+        creates layers of a neural network
+        :return: outputs a dictionary with keys layer names and values with objects of the LayerNN class
         """
         n_input_arr = []
         n_input_arr.append(n_input)
@@ -201,29 +224,45 @@ class MultilayerNN:
             n_input_arr.append(param_key[0])
             layer = {key: LayerNN(n_input_arr[i], param_key[0], param_key[1], param_key[2])}
             self.neural_network.update(layer)
-            # print("_initialize_layers end")
-        # print(f"self.neural_network:{self.neural_network}")
         return self.neural_network
 
     def forward(self, X):
+        """
+        calculates the output of a neural network
+        :param X: input data vector
+        :return: the vector of values that the neural network outputs
+        """
         a = X
         for i, key in enumerate(self.param_neural_network.keys(), 0):
             a = self.neural_network[key].forward(a)
         self.outputNN = a
-        return self.outputNN  # neural network output
+        return self.outputNN
 
     def __call__(self, X):
         return self.forward(X)
 
     def backward(self, dL):
+        """
+        passes the gradient of the function from one layer to another for back propagation
+        :param dL: the value of the gradient of the loss function
+        :return: None
+        """
         dA = dL
         for i, key in enumerate(reversed(self.neural_network.keys()), 0):
-            # print(f"{key}: ")
             dA = self.neural_network[key].backward(dA)
 
 
 class Loss:
+    """
+    Loss class that counts the loss function, forward and backward of model neural_network
+    """
+
     def __init__(self, model, loss_fn, loss_fn_bw):
+        """
+        :param model: model neural_network
+        :param loss_fn: loss function
+        :param loss_fn_bw:derivative of the loss function
+        """
         self.model = model
         self.loss_fn = loss_fn
         self.loss_fn_bw = loss_fn_bw
@@ -232,6 +271,11 @@ class Loss:
         self.y = None
 
     def __call__(self, a, y):
+        """
+        :param a: vector of predictions
+        :param y: vector of observed
+        :return: the value of the MSE function
+        """
         self.a = a
         self.y = y
         return self.loss_fn(a, y)
@@ -240,6 +284,10 @@ class Loss:
         return self.__call__(a, y)
 
     def backward(self, **params):
+        """
+        passes dL to back propagation
+        :return: vector of values of the gradient of the loss function
+        """
         assert (self.a is not None) and (self.y is not None), "loss.forward() must be called first!"
         self.dL = self.loss_fn_bw(self.a, self.y)
         self.model.backward(self.dL, **params)
@@ -247,6 +295,11 @@ class Loss:
 
 
 def sigmoid(z):
+    """
+    sigmoid function
+    :param z: vector of values
+    :return: the value of the sigmoid function
+    """
     z = np.float64(z)
     return np.where(
         z >= 0,
@@ -256,57 +309,83 @@ def sigmoid(z):
 
 
 def deriv_sigmoid(z):
+    """
+    derivative of the sigmoid function
+    :param z: vector of values
+    :return: the gradient value of the sigmoid
+    """
     z = np.float64(z)
     return sigmoid(z) * (1. - sigmoid(z))
 
 
 def mse_func(a, y):
+    """
+    MSE(mean squared error) function
+    :param a: vector of predictions
+    :param y: vector of observed
+    :return: the value of the MSE function
+    """
     return ((y - a) ** 2).mean()
 
 
-def deriv_mse_func(a, y, eps=1e-5):
+def deriv_mse_func(a, y):
+    """
+    derivative of the MSE(mean squared error) function
+    :param a: vector of predictions
+    :param y: vector of observed
+    :return: the gradient value of the MSE function
+    """
     return -1. / len(a) * (y - a) * 2
 
 
-# program body
+"""program body"""
+
+
 def main():
     # neural_network = {"layer_1": [100, np.tanh, lambda x: 1 - np.tanh(x) ** 2],
     #                   'last_layer_2': [1, sigmoid, deriv_sigmoid]}
-    # работает если в слоях одинаковое количество нейронов
-    # neural_network = {"layer_1": [16, sigmoid, deriv_sigmoid], 'layer_2': [32, sigmoid, deriv_sigmoid],
+    # neural_network = {"layer_1": [16, sigmoid, deriv_sigmoid],
+    #                   'layer_2': [32, sigmoid, deriv_sigmoid],
     #                   'last_layer_3': [1, sigmoid, deriv_sigmoid]}
-    neural_network = {"layer_1": [50, np.tanh, lambda x: 1 - np.tanh(x) ** 2],
-                      'layer_2': [100, np.tanh, lambda x: 1 - np.tanh(x) ** 2],
-                      'layer_3': [100, np.tanh, lambda x: 1 - np.tanh(x) ** 2],
+    neural_network = {"layer_1": [40, np.tanh, lambda x: 1 - np.tanh(x) ** 2],
+                      'layer_2': [30, np.tanh, lambda x: 1 - np.tanh(x) ** 2],
+                      'layer_3': [20, np.tanh, lambda x: 1 - np.tanh(x) ** 2],
                       'last_layer_4': [1, sigmoid, deriv_sigmoid]}
-    dataset = Dataset(point=1000, dimensionality=2, petals=4, radius=6, random_seed=1)
+    dataset = Dataset(point=600, dimensionality=2, petals=4, radius=6, random_seed=1)
     X, y = dataset.X, dataset.Y
     X_train, X_test, y_train, y_test = train_test_split(X, y, shuffle=True)
-    # dataset.show_start_graph()
+    dataset.show_start_graph()
     model = MultilayerNN(neural_network, n_input=2, )
-    optimizer = Optimizer(model, lr=1e-2)
+    optimizer = Optimizer(model, lr=1e-4)
     loss = Loss(model, mse_func, deriv_mse_func)
-    num_epochs = 25000
+    x_list_plt = []
+    y_list_plt = []
+    num_epochs = 100000
     for i in range(num_epochs):
         a = model.forward(X_train)
         l = loss(a, y_train)
-        # print(model.neural_network["layer_1"].cache["a1"] - model.neural_network["layer_2"].cache["X"])
-        dL = loss.backward()
+        loss.backward()
         optimizer.step()
         optimizer.zero_grad()
-        # print('end')
-        if (i % 400 == 0):
+        if ((i % 400 == 0)):
+            x_list_plt.append(i)
+            y_list_plt.append(l)
             a_val = model(X_test)
             print("Epoch %d/%d\t Loss: %.3f" % (i, num_epochs, l), end='\t')
             print("Accuracy: %.3f" % (accuracy_score(y_train, a > 0.5)), end='\t')
             print("Val_loss: %.3f" % (loss(a_val, y_test)), end='\t')
             print("Val_accuracy: %.3f" % (accuracy_score(y_test, a_val > 0.5)))
 
+    'сильно лагает когда грузит график'
     print("Decision Boundary for train, hidden layer size ")
-    plot_decision_boundary(lambda x: model(x) > 0.5, X_train, y_train)
-
-    print("Decision Boundary for test, hidden layer size ")
-    plot_decision_boundary(lambda x: model(x) > 0.5, X_test, y_test)
+    plt.ylabel('loss')
+    plt.xlabel('epochs')
+    plt.plot(x_list_plt,y_list_plt)
+    plt.show()
+    plot_decision_boundary(lambda x: model(x) > 0.5, X_train, y_train, dimensionality=2)
+    #
+    # print("Decision Boundary for test, hidden layer size ")
+    # plot_decision_boundary(lambda x: model(x) > 0.5, X_test, y_test)
 
 
 if __name__ == '__main__':
